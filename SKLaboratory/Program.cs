7f1388988ff142dcda90aa00b552db1f80d9fa48
AlexRadu <radualexandrucosmin@gmail.com>
2023-07-08 10:51:03 +0300

    The Program class uses a ServiceCollection to register the services it needs. It then builds a ServiceProvider and uses it to get the services. This way, the Program class doesn't need to know how to create the services, it just asks for them. This makes the code more flexible, as you can easily swap out services for different implementations or mock objects for testing.

--- SKLaboratory/Program.cs
+++ SKLaboratory/Program.cs
@@ -1,5 +1,7 @@
-﻿using SKLaboratory.ApplicationLifecycle;
+﻿using Microsoft.Extensions.DependencyInjection;
+using SKLaboratory.ApplicationLifecycle;
 using SKLaboratory.Factories;
+using SKLaboratory.Infrastructure.Interfaces;
 using SKLaboratory.Initialization;
 using StereoKit;
 
@@ -14,6 +16,26 @@ internal class Program
 
     static void Main(string[] args)
     {
+
+        // Create a new service collection
+        var serviceCollection = new ServiceCollection();
+
+        // Register your services
+        serviceCollection.AddSingleton<IWidgetFactory, WidgetFactory>();
+        serviceCollection.AddSingleton<WidgetManager>();
+        serviceCollection.AddSingleton<StereoKitInitializer>();
+        serviceCollection.AddSingleton<StartupWidgetActivator>();
+        serviceCollection.AddSingleton<MainAppLoop>();
+
+        // Build the service provider
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        // Use the service provider to get your services
+        var stereoKitInitializer = serviceProvider.GetRequiredService<StereoKitInitializer>();
+        var widgetCreator = serviceProvider.GetRequiredService<StartupWidgetActivator>();
+        var mainLoop = serviceProvider.GetRequiredService<MainAppLoop>();
+
+        // Use the services
         stereoKitInitializer.Initialize();
         widgetCreator.CreateWidgets();
         mainLoop.Run();
