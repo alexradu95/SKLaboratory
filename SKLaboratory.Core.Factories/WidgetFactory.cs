402a782860f44be637d36a21bb40327831fb065f
Radu Alexandru <Radu.Alexandru@expensya.com>
2023-09-28 21:38:54 +0300

    refactor

--- SKLaboratory.Core.Factories/WidgetFactory.cs
+++ SKLaboratory.Core.Factories/WidgetFactory.cs
@@ -10,8 +10,10 @@ public class WidgetFactory : IWidgetFactory
 
     private readonly MessageBus _messageBus;
 
+    // Constructor
     public WidgetFactory(MessageBus messageBus) => _messageBus = messageBus;
 
+    // Create a widget instance of the specified type
     public IWidget CreateWidget(Type widgetType)
     {
         if (_widgetCreators.TryGetValue(widgetType, out var createWidgetFunc))
@@ -28,32 +30,37 @@ public class WidgetFactory : IWidgetFactory
 
         throw new UnknownWidgetTypeException($"No widget registered for type {widgetType}.");
     }
+
+    // Register a widget type with the factory
     public void RegisterWidget<T>() where T : IWidget
     {
         Type widgetType = typeof(T);
         if (!typeof(IWidget).IsAssignableFrom(widgetType))
             throw new ArgumentException($"Type must be a subclass of IWidget, but was {widgetType}", nameof(widgetType));
 
-        Func<IWidget> createWidgetFunc = () =>
-        {
-            var constructorWithMessageBus = widgetType.GetConstructor(new[] { typeof(MessageBus) });
-            if (constructorWithMessageBus != null)
-            {
-                return (IWidget)constructorWithMessageBus.Invoke(new object[] { _messageBus });
-            }
-
-            var defaultConstructor = widgetType.GetConstructor(Type.EmptyTypes);
-            if (defaultConstructor != null)
-            {
-                return (IWidget)defaultConstructor.Invoke(null);
-            }
-
-            throw new InvalidOperationException($"Type must have a default constructor.");
-        };
+        Func<IWidget> createWidgetFunc = () => CreateWidgetInstance(widgetType);
 
         if (_widgetCreators.ContainsKey(widgetType))
             throw new ArgumentException($"A widget of type {widgetType} is already registered.", nameof(widgetType));
 
         _widgetCreators[widgetType] = createWidgetFunc;
+    }
+
+    // Create a widget instance using the appropriate constructor
+    private IWidget CreateWidgetInstance(Type widgetType)
+    {
+        var constructorWithMessageBus = widgetType.GetConstructor(new[] { typeof(MessageBus) });
+        if (constructorWithMessageBus != null)
+        {
+            return (IWidget)constructorWithMessageBus.Invoke(new object[] { _messageBus });
+        }
+
+        var defaultConstructor = widgetType.GetConstructor(Type.EmptyTypes);
+        if (defaultConstructor != null)
+        {
+            return (IWidget)defaultConstructor.Invoke(null);
+        }
+
+        throw new InvalidOperationException($"Type must have a default constructor.");
     }
 }
